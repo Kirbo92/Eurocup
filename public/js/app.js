a95b6647ac1523d05165e10b8c1695bb712a4d67
Fernández Carillo, Miguel Ángel <cociocibil@ono.com>
2016-05-18 19:27:09 +0200

    Add bootstrap layout

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,12 @@
 
 	app.controller('PlayerController', function () {
 		this.player = {
-			name: 'Casillas',
+			name: 'Iker Casillas Fernandez',
+			surname: 'Casillas',
 			position: 'Portero',
+			age: '34 años',
+			country: 'Spain',
+			number: '#01',
 			teams: ['Real Madrid C.F.', 'FC Oporto'],
 			height: "6´07´´ (1.85m)",
 			weight: "185.2 lbs (84 kg)"
